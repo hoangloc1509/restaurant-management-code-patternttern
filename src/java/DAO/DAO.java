dd31a761c203861925b7ff3a728d3af31d2b9ed9
Hoang Loc <hoanggdloc@gmail.com>
2022-01-03 23:30:19 +0700

    update DAO

--- src/java/DAO/DAO.java
+++ src/java/DAO/DAO.java
@@ -14,15 +14,16 @@ import java.sql.SQLException;
  */
 public class DAO {
     public static Connection con;
+    
     public DAO() {
-        if(con == null){
-            String dbUrl = 
-                "jdbc:mysql://localhost:3307/hotel?autoReconnect=true&useSSL=false";
-            String dbClass = "com.mysql.jdbc.Driver";
+        if(con==null) {
+            String dbUrl = "jdbc:mysql://localhost:3306/restaurant_management";
+            String dbClass = "com.mysql.cj.jdbc.Driver";
+            
             try {
                 Class.forName(dbClass);
-                con = DriverManager.getConnection (dbUrl, "root", "xxx");
-            } catch(ClassNotFoundException | SQLException e) {
+                con = DriverManager.getConnection(dbUrl,"root","");
+            }catch(ClassNotFoundException | SQLException e) {
             }
         }
     }
